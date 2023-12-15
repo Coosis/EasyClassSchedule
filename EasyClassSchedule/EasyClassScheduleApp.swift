a037ae4128d9e6436b47ce2e4dd6887ec84063e8
Coosis <1159727122@qq.com>
2023-12-15 15:30:08 +0800

    Initial Commit

---
+++ EasyClassSchedule/EasyClassScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyClassScheduleApp.swift
+//  EasyClassSchedule
+//
+//  Created by Vladimir Brooks on 2023/12/15.
+//
+
+import SwiftUI
+
+@main
+struct EasyClassScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
